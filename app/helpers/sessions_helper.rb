ccb1baf8aa726055a5b54c445cbb9eb061306c5a
jtoews32 <jtoews32@gmail.com>
2015-12-06 13:27:06 -0800

    update app

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,25 @@
 module SessionsHelper
   
   
-  def sign in(user)
+  def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     # cookies[:expires] = 20.years.from_now.utc is what permanet does
     self.current_user = user
    end
 
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  
+  def current_user=(user)
+    # create the instance var
+    @current_user = user
+  end
+  
+  def current_user
+     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
 
 end
